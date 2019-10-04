d0f328dcd4c4620c767a526df49743d7fcec0dbb
ktkay <kt2593@columbia.edu>
2019-10-04 06:40:33 -0400

    Add working content

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onTap(_ sender: Any) {
+        print("Hello")
+        
+        view.endEditing(true) // dismiss the keyboard when tapping
+    }
+    
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        //Get the bill amount
+        let bill = Double(billField.text!) ?? 0 // if it's not                                         //valid then it's 0
+        
+        //Calculate the tip and total
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //Update the tip and total label
+        tipLabel.text = String (format: "$%.2f", tip) // display tip
+        totalLabel.text = String (format: "$%.2f", total) // display total
+    }
+    
+    
 }
 
